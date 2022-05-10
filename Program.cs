bc2fdb9b145cc789db6104ac2f2b85b78dfe95d8
sanh nguyen <sanhnguyen734@gmail.com>
2022-05-10 13:53:30 +0700

    1

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5;
+            Console.Write(a);
+        }
+    }
+}
